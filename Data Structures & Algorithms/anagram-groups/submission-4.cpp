631de4b0180ad318d15d644b15e5c4906453fea5
Daniel Kim <72123261+agent-platypus@users.noreply.github.com>
2026-09-19 01:29:54 -0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string, vector<string>> freq;
+        vector<vector<string>> res;
+
+        for(const string& curr: strs) {
+            string s = curr;
+            sort(s.begin(), s.end());
+            freq[s].push_back(curr);
+        }
+
+        for(auto& i: freq) {
+            res.push_back(i.second);
+        }
+
+        return res;
+       
+    }
+};
