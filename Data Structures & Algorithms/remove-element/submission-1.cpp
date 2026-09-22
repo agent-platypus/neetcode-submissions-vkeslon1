032fe9afe618cdc71cc247f6d0f92e77a5dc055f
Daniel Kim <72123261+agent-platypus@users.noreply.github.com>
2026-09-21 20:00:30 -0700

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+
+        int counter{};
+        remove_if(nums.begin(), nums.end(), [&counter, val](int elem) { 
+            if (elem == val) {
+                return true;
+            }
+            ++counter;
+            return false;
+        });
+
+        for_each(nums.begin(), nums.end(), [](int a) { cout << a << "\n";});
+        return counter;
+
+        
+    }
+};
