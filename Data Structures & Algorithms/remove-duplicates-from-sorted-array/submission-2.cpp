64a9ba3a1d9f01625a92bd0d6dfae3751d15b669
Daniel Kim <72123261+agent-platypus@users.noreply.github.com>
2026-09-23 12:44:13 -0700

    Add: remove-duplicates-from-sorted-array - submission-2

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-2.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        // int left{};
+        // int unique_elem{nums.front()};
+        // for(int i{1}; i < nums.size(); ++i) {
+
+        //     if (nums[i] != unique_elem) {
+        //         nums[left + 1] = nums[i];
+        //         ++left;
+        //         unique_elem = nums[i];
+        //     }
+
+        // }
+        // return ++left;
+
+
+        auto it = unique(nums.begin(), nums.end());
+
+        nums.erase(it, nums.end());
+        return nums.size();
+    }
+};
+
+// 1   1   1  2   2  3   3  4 
+
+// lp         i
+
+//  1   2  1  2  2   3
+//     lp         i   i
+
+//   1  2  3  2  2  3  4
+//           lp
+
+///  1  2   3  4 
+
+
+// lp   i
+// 1  1   2
+// lp  i
+// 1  2 
+// lp i
+
